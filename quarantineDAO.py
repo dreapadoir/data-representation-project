4328223d24ec979152762ae4f70794100f18a5fc
David Higgins <g00411302@gmit.ie>
2023-12-03 21:03:23 +0000

    add DAO file

---
+++ quarantineDAO.py
@@ -0,0 +1,77 @@
+import mysql.connector
+
+class QuarantineDAO:
+    def __init__(self):
+        self.host = "localhost"
+        self.user = "root"
+        self.password = "root"
+        self.database = "quarantine"
+        self.connection = None
+        self.cursor = None
+
+    def getCursor(self):
+        if not self.connection or not self.connection.is_connected():
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+    
+    def closeAll(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+
+    def create(self, values):
+        cursor = self.getCursor()
+        sql = "insert into quar (lot, part, qty, datein, reason, badge, badgeout, dateout, status, signoutcomment) values (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newid = cursor.lastrowid
+        self.closeAll()
+        return newid
+    
+    def getAll(self):
+        cursor = self.getCursor()
+        sql="select * from quar"
+
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for x in result:
+            print(x)
+        self.closeAll()
+
+    def findByID(self, lot):
+        cursor = self.getCursor()
+        sql="select * from quar where id = %s"
+        values = (lot,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        print(result)
+        self.closeAll()
+
+    def update(self, values):
+        cursor = self.getCursor()
+        sql = "update quar set badgeout = %s, dateout = %s, status = %s, signoutcomment = %s where lot = %s"
+        cursor.execute(sql, values)
+        self.connection.commit()
+        print("update done")
+        self.closeAll()
+
+    def delete(self, lot):
+        cursor = self.getCursor()
+        sql = "delete from quar where lot = %s"
+        values = (lot,)
+        cursor.execute(sql, values)
+        self.connection.commit()
+        print("delete done")
+        self.closeAll()
+
+
+quarantineDAO = QuarantineDAO()
